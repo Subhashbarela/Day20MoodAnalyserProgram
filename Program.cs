fb8f5208d3319f8892c9b3a53bea4169c8d59067
Subhash Barela <barelasubhash16@gmail.com>
2023-03-03 00:20:37 +0530

    mood analyser program

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProgragram
+{
+    internal class Program
+    {
+      
+            public static void Main(string[] args)
+            {
+                string message = "I am happy";
+
+                AnalyzeMood(message);
+
+            }
+
+            public static void AnalyzeMood(string message)
+            {
+                string happy = "I am happy";
+                string sad = "I am sad";
+
+                if (message == happy)
+                {
+                    Console.WriteLine("Mood: Happy");
+                }
+                else if (message == sad)
+                {
+                    Console.WriteLine("Mood: Sad");
+                }
+            }
+        
+    }
+}
