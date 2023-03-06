0d1e702c1878686182d728ed240d3119a57b6249
Subhash Barela <barelasubhash16@gmail.com>
2023-03-06 09:54:15 +0530

    mood analysis program using exception

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static MoodAnalyserProgragram.MoodAnalyse;
 
 namespace MoodAnalyserProgragram
 {
@@ -16,10 +15,10 @@ namespace MoodAnalyserProgragram
 
             try
             {
-                AnalyzeMood mood = new AnalyzeMood("");
+                MoodAnalyze mood = new MoodAnalyze("");
                 mood.Analyze();
             }
-            catch (MoodAnalyzeException ex)
+            catch (MoodAnalysisException ex)
             {
                 Console.WriteLine(ex.Message);
             }
